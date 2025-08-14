5c2196652c0a0de33de227494424d66ff47385be
Trung Dao <bomcon123456@gmail.com>
2025-08-14 07:50:23 +0700

    upppp

--- miryoku/mapping/38/totem.h
+++ miryoku/mapping/38/totem.h
@@ -9,11 +9,11 @@
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+     N30, N31, K32, K33, K34,      K35, K36, K37, K38, K39 \
 ) \
      K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
      K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
-K00  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K09 \
+K38  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K39 \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 33 34
